508f33525fbf2a58a5dd885d80c034b979a74789
SubhashKumar <subhash.kumar307@gmail.com>
2024-10-06 02:58:00 +0530

    Added dependency injection

--- productlist/src/main/java/com/bitsnbytes/product/service/CategoryService.java
+++ productlist/src/main/java/com/bitsnbytes/product/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.bitsnbytes.product.service;
 import com.bitsnbytes.product.dto.CategoryDTO;
 import com.bitsnbytes.product.entity.Category;
 import com.bitsnbytes.product.exception.CategoryAlreadyExistsException;
+import com.bitsnbytes.product.exception.CategoryNotFoundException;
 import com.bitsnbytes.product.mapper.CategoryMapper;
 import com.bitsnbytes.product.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class CategoryService {
 
 
@@ -36,7 +38,7 @@ public class CategoryService {
     }
     // get category by id
     public CategoryDTO getCategoryById(Long id){
-        Category category = categoryRepository.findById(id).orElseThrow(()-> new RuntimeException("Category not found"));
+        Category category = categoryRepository.findById(id).orElseThrow(()-> new CategoryNotFoundException("Category not found"));
         return CategoryMapper.toCategoryDTO(category);
     }
     // delete category
